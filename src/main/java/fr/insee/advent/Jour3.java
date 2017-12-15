a414da13710670dd497381d9707040600e9fbfe7
Romain Warnan <romain.warnan@gmail.com>
2017-12-15 09:26:06 +0100

    Calcul du rang numéro du carré sur lequel se trouve le nombre

--- src/main/java/fr/insee/advent/Jour3.java
+++ src/main/java/fr/insee/advent/Jour3.java
@@ -1,8 +1,30 @@
 package fr.insee.advent;
 
+import java.io.IOException;
+
 public class Jour3 {
 
+    public static void main(String[] args) throws IOException {
+        Jour3 jour = new Jour3();
+        System.out.println("Jour3");
+        System.out.println("1. " + jour.ex1(1));
+    }
+	
 	public int ex1(int n) {
 		return 0;
+	}
+	
+	private int rank(int p) {
+		if(p == 1) {
+			return 0;
+		}
+		if(p < 10) {
+			return 1;
+		}
+		int n = 1;
+		while ((8 * n + 2) > p || (16 * n + 9) < p) {
+			n ++;
+		}
+		return n + 1;
 	}
 }
