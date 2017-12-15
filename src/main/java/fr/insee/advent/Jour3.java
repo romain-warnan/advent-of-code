83ab64fc1b918a3cb5427885a6d88a3fd0855b23
Romain Warnan <romain.warnan@gmail.com>
2017-12-15 11:13:36 +0100

    Classe 'Point'

--- src/main/java/fr/insee/advent/Jour3.java
+++ src/main/java/fr/insee/advent/Jour3.java
@@ -6,11 +6,18 @@ public class Jour3 {
 
     public static void main(String[] args) throws IOException {
         Jour3 jour = new Jour3();
+        System.out.println(jour.rank(26));
         System.out.println("Jour3");
         System.out.println("1. " + jour.ex1(1));
     }
 	
-	public int ex1(int n) {
+	public int ex1(int p) {
+		int n = this.rank(p);
+		Point point = new Point(n);
+		int start = 8 * n + 2;
+		for(int k = start; k <= p; k ++) {
+			
+		}
 		return 0;
 	}
 	
@@ -26,5 +33,34 @@ public class Jour3 {
 			n ++;
 		}
 		return n + 1;
+	}
+	
+	static class Point {
+		int x, y;
+		
+		public Point(int n) {
+			x = n;
+			y = 1 - n;
+		}
+		
+		public void up() {
+			y = y + 1;
+		}
+		
+		public void down() {
+			y = y - 1;
+		}
+		
+		public void right() {
+			x = x + 1;
+		}
+		
+		public void left() {
+			x = x - 1;
+		}
+		
+		public int manhattan() {
+			return Math.abs(x) + Math.abs(y);
+		}
 	}
 }
