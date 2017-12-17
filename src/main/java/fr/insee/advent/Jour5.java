f08afd30386da5313a65149b82607f94aebe6e2e
Romain Warnan <romain.warnan@gmail.com>
2017-12-17 19:52:23 +0100

    Refactor Jour 5

--- src/main/java/fr/insee/advent/Jour5.java
+++ src/main/java/fr/insee/advent/Jour5.java
@@ -3,6 +3,7 @@ package fr.insee.advent;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.function.ToIntFunction;
 
 public class Jour5 {
 
@@ -13,7 +14,7 @@ public class Jour5 {
 		System.out.println("2. " + jour.ex2("src/main/resources/input5"));
 	}
 	
-	public long ex1(String path) throws IOException {
+	public long count(String path, ToIntFunction<Integer> function) throws IOException {
 		Integer[] numbers = Files
 			.readAllLines(Paths.get(path))
 			.stream()
@@ -23,32 +24,18 @@ public class Jour5 {
 		int index = 0;
 		while (index >= 0 && index < numbers.length) {
 			int jump = numbers[index];
-			numbers[index] ++;
+			numbers[index] = function.applyAsInt(numbers[index]);
 			index = index + jump;
 			count ++;
 		}
 		return count;
 	}
+	
+	public long ex1(String path) throws IOException {
+		return this.count(path, n -> n + 1);
+	}
 
 	public long ex2(String path) throws IOException {
-		Integer[] numbers = Files
-			.readAllLines(Paths.get(path))
-			.stream()
-			.map(Integer::valueOf)
-			.toArray(Integer[]::new);
-		int count = 0;
-		int index = 0;
-		while (index >= 0 && index < numbers.length) {
-			int jump = numbers[index];
-			if (jump >= 3) {
-				numbers[index] --;	
-			}
-			else {
-				numbers[index] ++;
-			}
-			index = index + jump;
-			count ++;
-		}
-		return count;
+		return this.count(path, n -> n >= 3 ? n - 1 : n + 1);
 	}
 }
