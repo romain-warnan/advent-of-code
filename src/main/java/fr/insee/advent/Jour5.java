a46ad920b456c7c69eef261446fbdcb5334c015a
Romain Warnan <romain.warnan@gmail.com>
2017-12-17 16:54:00 +0100

    Mise en place Jour 5

---
+++ src/main/java/fr/insee/advent/Jour5.java
@@ -0,0 +1,10 @@
+package fr.insee.advent;
+
+
+public class Jour5 {
+
+	public long ex1(String path) {
+		return 0;
+	}
+
+}
---
+++ src/test/java/fr/insee/advent/Jour5Test.java
@@ -0,0 +1,13 @@
+package fr.insee.advent;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Jour5Test {
+
+    @Test
+    public void test1() throws Exception {
+        Jour5 jour = new Jour5();
+        Assert.assertEquals(5, jour.ex1("src/test/resources/input5"));
+    }
+}
