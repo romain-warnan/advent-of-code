e670bef520691b509e179a4bd22c697ae492b6b8
Romain Warnan <romain.warnan@gmail.com>
2017-12-17 19:37:24 +0100

    Jour 5, première version.

--- src/main/java/fr/insee/advent/Jour5.java
+++ src/main/java/fr/insee/advent/Jour5.java
@@ -1,10 +1,32 @@
 package fr.insee.advent;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Jour5 {
 
-	public long ex1(String path) {
-		return 0;
+	public static void main(String[] args) throws IOException {
+		Jour5 jour = new Jour5();
+		System.out.println("Jour5");
+		System.out.println("1. " + jour.ex1("src/main/resources/input5"));
+	}
+	
+	public long ex1(String path) throws IOException {
+		Integer[] numbers = Files
+			.readAllLines(Paths.get(path))
+			.stream()
+			.map(Integer::valueOf)
+			.toArray(Integer[]::new);
+		int count = 0;
+		int index = 0;
+		while (index >= 0 && index < numbers.length) {
+			int jump = numbers[index];
+			numbers[index] ++;
+			index = index + jump;
+			count ++;
+		}
+		return count;
 	}
 
 }
