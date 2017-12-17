345faaf3b6d03b642c78252ec2a8a6eb6fecdb3c
Romain Warnan <romain.warnan@gmail.com>
2017-12-17 21:32:35 +0100

    Jour 6 solution 1

--- src/main/java/fr/insee/advent/Jour6.java
+++ src/main/java/fr/insee/advent/Jour6.java
@@ -8,19 +8,19 @@ import java.util.List;
 public class Jour6 {
 
 	public static void main(String[] args) throws IOException {
-		Jour5 jour = new Jour5();
-		System.out.println("Jour5");
-		System.out.println("1. " + jour.ex1("11	11	13	7	0	15	5	5	4	4	1	1	7	1	15	11"));
+		Jour6 jour = new Jour6();
+		System.out.println("Jour6");
+		System.out.println("1. " + jour.ex1("11 11 13 7 0 15 5 5 4 4 1 1 7 1 15 11"));
 	}
 
 	public long ex1(String input) {
 		List<Memory> memories = new ArrayList<>();
 		memories.add(this.firstMemory(input));
 		int n = 0;
-		while(true) {
-			n ++;
+		while (true) {
+			n++;
 			Memory memory = this.reallocate(memories.get(memories.size() - 1));
-			if (memories.contains(memory)){
+			if (memories.contains(memory)) {
 				return n;
 			}
 			memories.add(memory);
@@ -38,12 +38,27 @@ public class Jour6 {
 	private Memory reallocate(Memory memory) {
 		return new Memory(this.reallocate(memory.banks));
 	}
-	
-	private int[] reallocate(int[] banks) {
-		// TODO code
+
+	private int[] reallocate(int[] input) {
+		int[] banks = Arrays.copyOf(input, input.length);
+		int max = Arrays.stream(banks).max().getAsInt();
+		int indexOfMax = this.firstIndexOf(banks, max);
+		banks[indexOfMax] = 0;
+		for (int n = 0; n < max; n++) {
+			banks[(indexOfMax + n + 1) % banks.length] ++;
+		}
 		return banks;
 	}
 
+	private int firstIndexOf(int[] array, int number) {
+		for (int n = 0; n < array.length; n++) {
+			if (array[n] == number) {
+				return n;
+			}
+		}
+		return -1;
+	}
+	
 	public static class Memory {
 
 		private int[] banks;
