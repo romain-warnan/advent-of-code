4823a5d285e4ea57939ba10f5ead3d797c418ea9
Romain Warnan <romain.warnan@gmail.com>
2017-12-20 16:22:22 +0100

    Fonction de nettoyage de la chaine de caractère

--- src/main/java/fr/insee/advent/Jour9.java
+++ src/main/java/fr/insee/advent/Jour9.java
@@ -15,6 +15,40 @@ public class Jour9 {
 	}
 	
 	public long ex1(String input) {
+		this.removeAllGarbage(input);
 		return -1;
+	}
+	
+	public String removeAllGarbage(String input) {
+		String output = this.removeFirstGarbage(input);
+		if(output.equals(input)) {
+			return output;
+		}
+		return this.removeAllGarbage(output);
+	}
+	
+	private String removeFirstGarbage(String input) {
+		char[] chars = input.toCharArray();
+		int beginIndex = -1, endIndex = -1;
+		for (int n = 0; n < chars.length; n++) {
+			char ch = chars[n];
+			if(ch == '!') {
+				n ++;
+			}
+			else {
+				if(ch == '<') {
+					beginIndex = n;
+				}
+				if(beginIndex >= 0 && ch == '>') {
+					endIndex = n + 1;
+					break;
+				}
+			}
+		}
+		if(endIndex >= 0) {
+    		String garbage = input.substring(beginIndex, endIndex);
+    		return input.replace(garbage, "");
+		}
+		return input;
 	}
 }
