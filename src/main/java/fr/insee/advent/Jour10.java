bbcf1ba0cedcb49542ac49eafac298736ddcfa60
Romain Warnan <romain.warnan@gmail.com>
2017-12-21 16:04:46 +0100

    Début du code 10-1

--- src/main/java/fr/insee/advent/Jour10.java
+++ src/main/java/fr/insee/advent/Jour10.java
@@ -1,9 +1,12 @@
 package fr.insee.advent;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.IntFunction;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Jour10 {
 
@@ -14,10 +17,25 @@ public class Jour10 {
 	}
 	
 	public long ex1(int size, String input) {
+		List<Integer> marks = IntStream.range(0, 256)
+			.mapToObj(n -> n)
+			.collect(Collectors.toList());
 		List<Integer> numbers = Arrays.stream(input.split(","))
 			.map(Integer::valueOf)
 			.collect(Collectors.toList());
-		
+		int index = 0;
+		for (int skip = 0; skip <= 0; skip ++) {
+//		for (int skip = 0; skip < numbers.size(); skip ++) {
+			List<Integer> copyOfMarks = new ArrayList<>(marks);
+			int length = numbers.get(skip);
+			for (int p = 0; p < size; p ++) {
+				if(index <= p && p < index + length) {
+					marks.set(index, copyOfMarks.get(length - p));
+				}
+			}
+			index = (index + skip) % size;
+		}
+		System.out.println(marks);
 		return 0L;
 	}
 
