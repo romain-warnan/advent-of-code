5e6db7e8e13e9b0dc9d307db96b0405f084f9f32
Romain Warnan <romain.warnan@gmail.com>
2017-12-27 17:46:34 +0100

    Jour 13, partie 1. Propre.

--- src/main/java/fr/insee/advent/Jour13.java
+++ src/main/java/fr/insee/advent/Jour13.java
@@ -1,10 +1,91 @@
 package fr.insee.advent;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Jour13 {
 
-	public long ex1(String path) {
-		return 0L;
+	public static void main(String[] args) throws Exception {
+		Jour13 jour = new Jour13();
+		System.out.println("Jour13");
+		System.out.println("1. " + jour.ex1("src/main/resources/input13"));
+	}
+	
+	public long ex1(String path) throws Exception {
+		Firewall firewall = new Firewall(path);
+		for (int n = 0; n < 100; n ++) {
+			firewall.nextStep();
+		}
+		return firewall.severity;
 	}
 
+	private static class Layer {
+		
+		int depth, range, scanner;
+		boolean up;
+
+		private Layer(int depth, int range) {
+			super();
+			this.depth = depth;
+			this.range = range;
+			this.scanner = 0;
+			this.up = false;
+		}
+		
+		public static Layer fromLine(String line) {
+			line = line.replaceAll(" +", "");
+			String[] tokens = line.split(":");
+			int depth = Integer.valueOf(tokens[0]);
+			int range = Integer.valueOf(tokens[1]);
+			return new Layer(depth, range);
+		}
+		
+		public void scanNext() {
+			if (up) {
+				scanner --;
+			}
+			else {
+				scanner ++;
+			}
+			if (scanner == 0 || scanner == range - 1) {
+				up = !up;
+			}
+		}
+		
+		public int severity(int packet) {
+			if (packet == this.depth && this.scanner == 0) {
+				return this.depth * this.range;
+			}
+			return 0;
+		}
+	}
+	
+	private static class Firewall {
+		
+		Map<Integer, Layer> layers;
+		int packet, severity;
+		
+		public Firewall(String path) throws Exception {
+			this.packet = 0;
+			this.severity = 0;
+			this.layers = Files.readAllLines(Paths.get(path))
+				.stream()
+				.map(Layer::fromLine)
+				.collect(Collectors.toMap(layer -> layer.depth, Function.identity()));
+		}
+		
+		public void nextStep() {
+			Layer layer = layers.get(packet);
+			if (layer != null) {
+				severity = severity + layer.severity(packet);
+			}
+			packet ++;
+			layers.values()
+				.stream()
+				.forEach(Layer::scanNext);
+		}
+	}
 }
