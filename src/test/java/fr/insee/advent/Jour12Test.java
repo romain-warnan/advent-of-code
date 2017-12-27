02c0d62b061f8e62885d5eb29bde3cb62858e817
Romain Warnan <romain.warnan@gmail.com>
2017-12-27 15:17:33 +0100

    Jour 12, 2e partie

--- src/test/java/fr/insee/advent/Jour12Test.java
+++ src/test/java/fr/insee/advent/Jour12Test.java
@@ -7,9 +7,14 @@ public class Jour12Test {
 
 
     @Test
-    public void test11() throws Exception {
+    public void test1() throws Exception {
    	 Jour12 jour = new Jour12();
    	 Assert.assertEquals(6, jour.ex1("src/test/resources/input11"));
     }
    
+    @Test
+    public void test2() throws Exception {
+   	 Jour12 jour = new Jour12();
+   	 Assert.assertEquals(2, jour.ex2("src/test/resources/input11"));
+    }
 }
