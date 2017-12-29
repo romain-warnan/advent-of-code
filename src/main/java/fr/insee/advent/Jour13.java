5002e57236df5ef6a89bc621b23da192700ff19d
Romain Warnan <romain.warnan@gmail.com>
2017-12-29 14:01:29 +0100

    Import en trop.

--- src/main/java/fr/insee/advent/Jour13.java
+++ src/main/java/fr/insee/advent/Jour13.java
@@ -2,7 +2,6 @@ package fr.insee.advent;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
