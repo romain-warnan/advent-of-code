b19f37eb7c727e1b72d5ede73f5e01d10c547e8d
Romain Warnan <romain.warnan@gmail.com>
2018-01-01 12:09:51 +0100

    Jour 15, partie 2. Rien à dire.

--- src/main/java/fr/insee/advent/Jour15.java
+++ src/main/java/fr/insee/advent/Jour15.java
@@ -14,6 +14,7 @@ public class Jour15 {
 		Jour15 jour = new Jour15();
 		System.out.println("Jour15");
 		System.out.println("1. " + jour.ex1(40_000_000, 116, 299));
+		System.out.println("2. " + jour.ex2(5_000_000, 116, 299));
 	}
 	
 	public long ex1(long numberOfPairs, long seedForA, long seedForB) {
@@ -36,5 +37,35 @@ public class Jour15 {
 	
 	private static long nextB(long b) {
 		return (b * generatorForB) % divider; 
+	}
+
+	public long ex2(long numberOfPairs, long seedForA, long seedForB) {
+		long result = 0;
+		long a = nextWithCriteriaA(seedForA);
+		long b = nextWithCriteriaB(seedForB);
+		for (long n = 0; n < numberOfPairs; n ++) {
+			if (((a ^ b) & bitmask) == 0) {
+				result ++;
+			}
+			a = nextWithCriteriaA(a);
+			b = nextWithCriteriaB(b);
+		}
+		return result;
+	}
+
+	private static long nextWithCriteriaA(long a) {
+		long nextA = nextA(a);
+		if (nextA % 4 == 0) {
+			return nextA;
+		}
+		return nextWithCriteriaA(nextA);
+	}
+	
+	private static long nextWithCriteriaB(long b) {
+		long nextB = nextB(b);
+		if (nextB % 8 == 0) {
+			return nextB;
+		}
+		return nextWithCriteriaB(nextB);
 	}
 }
--- src/test/java/fr/insee/advent/Jour15Test.java
+++ src/test/java/fr/insee/advent/Jour15Test.java
@@ -17,4 +17,17 @@ public class Jour15Test {
    	 Jour15 jour = new Jour15();
    	 Assert.assertEquals(588, jour.ex1(40_000_000, 65, 8_921));
     }
+    
+    @Test
+    public void test21() throws Exception {
+   	 Jour15 jour = new Jour15();
+   	 Assert.assertEquals(0, jour.ex2(1_055, 65, 8_921));
+   	 Assert.assertEquals(1, jour.ex2(1_056, 65, 8_921));
+    }
+    
+    @Test
+    public void test22() throws Exception {
+   	 Jour15 jour = new Jour15();
+   	 Assert.assertEquals(309, jour.ex2(5_000_000, 65, 8_921));
+    }
 }
