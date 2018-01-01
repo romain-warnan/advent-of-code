23288cf3bc5393c03710bfb1eedf6525046fbe79
Romain Warnan <romain.warnan@gmail.com>
2018-01-01 11:46:14 +0100

    Jour 15, partie 1. Au top ! Merci le jour 10 pour l’opérateur ^.

--- src/main/java/fr/insee/advent/Jour15.java
+++ src/main/java/fr/insee/advent/Jour15.java
@@ -3,14 +3,38 @@ package fr.insee.advent;
 
 public class Jour15 {
 
+	private static final int
+		generatorForA = 16_807,
+		generatorForB = 48_271,
+		divider = 2_147_483_647,
+		bitmask = 0b1111_1111_1111_1111
+	;
+	
 	public static void main(String[] args) throws Exception {
 		Jour15 jour = new Jour15();
 		System.out.println("Jour15");
 		System.out.println("1. " + jour.ex1(40_000_000, 116, 299));
 	}
 	
-	public long ex1(int numberOfPairs, int seedForA, int seedForB) {
-		return 0L;
+	public long ex1(long numberOfPairs, long seedForA, long seedForB) {
+		long result = 0;
+		long a = nextA(seedForA);
+		long b = nextB(seedForB);
+		for (long n = 0; n <= numberOfPairs; n ++) {
+			if (((a ^ b) & bitmask) == 0) {
+				result ++;
+			}
+			a = nextA(a);
+			b = nextB(b);
+		}
+		return result;
 	}
-
+	
+	private static long nextA(long a) {
+		return (a * generatorForA) % divider; 
+	}
+	
+	private static long nextB(long b) {
+		return (b * generatorForB) % divider; 
+	}
 }
