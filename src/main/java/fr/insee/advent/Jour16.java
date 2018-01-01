2071b8655ece84e457e6b8f1f678b742de45a113
Romain Warnan <romain.warnan@gmail.com>
2018-01-01 12:27:05 +0100

    Mise en place du jour 16.

---
+++ src/main/java/fr/insee/advent/Jour16.java
@@ -0,0 +1,17 @@
+package fr.insee.advent;
+
+
+public class Jour16 {
+
+	public static void main(String[] args) throws Exception {
+		Jour16 jour = new Jour16();
+		System.out.println("Jour16");
+		System.out.println("1. " + jour.ex1("abcdefghijklmnop", "src/main/resources/input16"));
+	}
+	
+	public String ex1(String programs, String path) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
