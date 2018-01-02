86e8d905ca01f3098f19b6b6e326052dc205be83
Romain Warnan <romain.warnan@gmail.com>
2018-01-02 09:41:12 +0100

    Jour 17, partie 2. Easy aussi.
    Rien à voir avec la partie 1 car on connait dès le début la position du
    0.

--- src/main/java/fr/insee/advent/Jour17.java
+++ src/main/java/fr/insee/advent/Jour17.java
@@ -9,6 +9,7 @@ public class Jour17 {
 		Jour17 jour = new Jour17();
 		System.out.println("Jour17");
 		System.out.println("1. " + jour.ex1(314));
+		System.out.println("2. " + jour.ex2(314));
 	}
 	
 	public int ex1(int steps) {
@@ -21,5 +22,21 @@ public class Jour17 {
 		}
 		int indexOf2017 = buffer.indexOf(2017);
 		return buffer.get((indexOf2017 + 1) % buffer.size());
+	}
+	
+	public int ex2(int steps) {
+		int currentPosition = 0;
+		int indexOfZero = 0;
+		int valueAfterZero = 0;
+		for (int n = 1; n < 50_000_000; n ++) {
+			currentPosition = ((currentPosition + steps) % n) + 1;
+			if(currentPosition == (indexOfZero + 1)) {
+				valueAfterZero = n;
+			}
+			else if (currentPosition <= indexOfZero) {
+				indexOfZero ++;
+			}
+		}
+		return valueAfterZero;
 	}
 }
