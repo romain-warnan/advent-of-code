347f7d90371acfa90d84357acd6be10d17a5aef3
Romain Warnan <romain.warnan@gmail.com>
2018-01-03 10:36:47 +0100

    Jour 19, chargement de la carte

--- src/main/java/fr/insee/advent/Jour19.java
+++ src/main/java/fr/insee/advent/Jour19.java
@@ -1,6 +1,11 @@
 package fr.insee.advent;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Jour19 {
 
@@ -11,6 +16,23 @@ public class Jour19 {
 	}
 
 	public String ex1(String path) throws IOException {
+		char[][] map = readMapFrom(path);
 		return null;
+	}
+	
+	static char[][] readMapFrom(String path) throws IOException {
+		List<char[]> lines = Files.readAllLines(Paths.get(path)).stream()
+				.map(String::toCharArray)
+				.collect(Collectors.toList());
+		int width = lines.get(0).length;
+		int height = lines.size();
+		char[][] map = new char[width][height];
+		for (int i = 0; i < width; i ++) {
+			for (int j = 0; j < height; j ++) {
+				map[i][j] = lines.get(j)[i];
+			}	
+		}
+		lines = null;
+		return map;
 	}
 }
