d23d5eab06297073f2e5e46a9d27ec6c2131dbdd
Romain Warnan <romain.warnan@gmail.com>
2018-01-05 18:31:02 +0100

    Implémentation des transformations

--- src/main/java/fr/insee/advent/Jour21.java
+++ src/main/java/fr/insee/advent/Jour21.java
@@ -3,7 +3,6 @@ package fr.insee.advent;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,18 +12,14 @@ public class Jour21 {
 
 	static final Pattern biRule = Pattern.compile("([.#]{2})/([.#]{2}) => ([.#]{3})/([.#]{3})/([.#]{3})");
 	static final Pattern triRule = Pattern.compile("([.#]{3})/([.#]{3})/([.#]{3}) => ([.#]{4})/([.#]{4})/([.#]{4})/([.#]{4})");
-	/**	
-		.#.
-		..#
-		###
-	*/
-	static final boolean[][] firstPattern = new boolean[][] {
-		{ false, true, false },
-		{ false, false, true },
-		{ true, true, true }
+	/**
+	 * .#.
+	 * ..#
+	 * ###
+	 */
+	static final boolean[][] firstPattern = new boolean[][] { { false, true, false }, { false, false, true }, { true, true, true }
 	};
-	
-	
+
 	public static void main(String[] args) throws IOException {
 		Jour21 jour = new Jour21();
 		System.out.println("Jour21");
@@ -32,56 +27,117 @@ public class Jour21 {
 	}
 
 	public int ex1(int iterations, String path) throws IOException {
-		List<Rule> rules = Files.readAllLines(Paths.get(path))
+		List<Rule> rules = Files
+			.readAllLines(Paths.get(path))
 			.stream()
 			.map(Rules::fromLine)
 			.collect(Collectors.toList());
 		Rule rule = rules.get(39);
 		System.out.println(rule);
-		rule.rotate();
+		rule.rotate270();
 		System.out.println(rule);
 		return -1;
 	}
-	
+
 	/**
-	 * '.' = 46 
+	 * '.' = 46
 	 * '#' = 35
 	 */
-	
+
 	interface Rule {
-		void rotate();
+
+		void rotate90();
+		void rotate180();
+		void rotate270();
+		void flipV();
+		void flipH();
 	}
-	
+
 	static abstract class Rules implements Rule {
-		
+
 		boolean[][] in;
 		boolean[][] out;
 
 		@Override
-		public void rotate() {
-			boolean[][] rotation = Arrays.copyOf(in, in.length);
-			for (int i = 0; i < in.length; i ++) {
-					for (int j = 0; j < in[0].length; j ++) {
-						rotation[i][j] = in[j][i];
-					}
+		public void rotate90() {
+			final int M = in.length;
+			final int N = in[0].length;
+			boolean[][] rotation = new boolean[N][M];
+			for (int r = 0; r < M; r++) {
+				for (int c = 0; c < N; c++) {
+					rotation[c][M - 1 - r] = in[r][c];
+				}
 			}
 			this.in = rotation;
 		}
 		
+		@Override
+		public void rotate180() {
+			final int M = in.length;
+			final int N = in[0].length;
+			boolean[][] rotation = new boolean[N][M];
+			for (int r = 0; r < M; r++) {
+				for (int c = 0; c < N; c++) {
+					rotation[M - r - 1][N - c - 1] = in[r][c];
+				}
+			}
+			this.in = rotation;
+		}
+		
+		@Override
+		public void rotate270() {
+			final int M = in.length;
+			final int N = in[0].length;
+			boolean[][] rotation = new boolean[N][M];
+			for (int r = 0; r < M; r++) {
+				for (int c = 0; c < N; c++) {
+					rotation[N - c - 1][r] = in[r][c];
+				}
+			}
+			this.in = rotation;
+		}
+		
+		@Override
+		public void flipV() {
+			final int M = in.length;
+			final int N = in[0].length;
+			boolean[][] rotation = new boolean[N][M];
+			for (int r = 0; r < M; r++) {
+				for (int c = 0; c < N; c++) {
+					rotation[r][M - 1 - c] = in[r][c];
+				}
+			}
+			this.in = rotation;
+		}
+		
+		@Override
+		public void flipH() {
+			final int M = in.length;
+			final int N = in[0].length;
+			boolean[][] rotation = new boolean[N][M];
+			for (int r = 0; r < M; r++) {
+				for (int c = 0; c < N; c++) {
+					rotation[N - 1 - r][c] = in[r][c];
+				}
+			}
+			this.in = rotation;
+		}
+
 		static boolean[] fromGroup(String group) {
-			Boolean[] boxed = group.chars()
+			Boolean[] boxed = group
+				.chars()
 				.boxed()
 				.map(n -> (n == 35))
 				.toArray(Boolean[]::new);
 			boolean[] pattern = new boolean[boxed.length];
-			for (int index = 0; index < boxed.length; index ++) {
+			for (int index = 0; index < boxed.length; index++) {
 				pattern[index] = boxed[index].booleanValue();
 			}
 			boxed = null;
 			return pattern;
 		}
-		
-		static Rule fromLine (String line) {
+
+		static Rule fromLine(String line) {
 			Matcher triMatcher = triRule.matcher(line);
 			if (triMatcher.matches()) {
 				return TriRule.from(triMatcher);
@@ -96,8 +152,8 @@ public class Jour21 {
 		@Override
 		public String toString() {
 			StringBuilder builder = new StringBuilder();
-			for (int i = 0; i < in.length; i ++) {
-				for (int j = 0; j < in[0].length; j ++) {
+			for (int i = 0; i < in.length; i++) {
+				for (int j = 0; j < in[0].length; j++) {
 					builder.append(in[i][j] ? '#' : '.');
 				}
 				builder.append('\n');
@@ -105,40 +161,39 @@ public class Jour21 {
 			return builder.toString();
 		}
 	}
-	
-	
+
 	static class BiRule extends Rules {
-		
+
 		BiRule() {
 			in = new boolean[2][2];
 			out = new boolean[3][3];
 		}
-		
+
 		static BiRule from(Matcher matcher) {
 			BiRule instance = new BiRule();
 			instance.in[0] = fromGroup(matcher.group(1));
 			instance.in[1] = fromGroup(matcher.group(2));
-			
+
 			instance.out[0] = fromGroup(matcher.group(3));
 			instance.out[1] = fromGroup(matcher.group(4));
 			instance.out[2] = fromGroup(matcher.group(5));
 			return instance;
 		}
 	}
-	
+
 	static class TriRule extends Rules {
-		
+
 		TriRule() {
 			in = new boolean[3][3];
 			out = new boolean[4][4];
 		}
-		
+
 		static TriRule from(Matcher matcher) {
 			TriRule instance = new TriRule();
 			instance.in[0] = fromGroup(matcher.group(1));
 			instance.in[1] = fromGroup(matcher.group(2));
 			instance.in[2] = fromGroup(matcher.group(3));
-			
+
 			instance.out[0] = fromGroup(matcher.group(4));
 			instance.out[1] = fromGroup(matcher.group(5));
 			instance.out[2] = fromGroup(matcher.group(6));
